c2d422dbc60d29a5e04702f6c292e297b7515ed6
Rodfc773 <rodrigo.fc773@gmail.com>
2023-09-11 10:13:21 -0300

    Criando o filtro da barra de pesquisa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,5 +10,11 @@
     <div id="search-container" class="col-md-12">
 
         <input type="text" id="search-input" class="form-control" placeholder="Pesquisar professores ou assunto...">
+
+        <div class="filter-container">
+            <label for="Filtro">Filtrar por </label>
+            <button class="assunto">Assunto</button>
+            <button class="Professor">Professor</button>
+        </div>
     </div>
 @endsection
