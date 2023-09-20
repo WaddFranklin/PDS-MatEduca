ba50f4773581889e7a637a10a903e1ba1e49c5c6
Rodfc773 <rodrigo.fc773@gmail.com>
2023-09-19 22:10:16 -0300

    Completando 75% da página do perfil do professor(faltando fazer a responsabilidade da página)

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -20,5 +20,39 @@
 
 <section class="more-informations">
 
+    <div class="resume">
+
+        <div class="resume-up">
+
+            <p class="resume-text">Acredito em tornar a matemática acessível e interessante para todos os alunos. Minhas aulas são interativas e focam em aplicar conceitos a situações do cotidiano.</p>
+
+            <p class="register-time">Inscrita desde 20 de fevereiro de 2019</p>
+        </div>
+
+        <hr />
+
+        <div class="resume-middle">
+
+            <p>Especialidades</p>
+
+            <ul class="special-subjects">
+                <div>
+                    <li>Álgebra</li>
+                </div>
+
+                <div>
+                    <li>Geometria</li>
+                </div>
+            </ul>
+        </div>
+
+        <hr>
+
+        <div class="resume-down">
+            <p class = "curr">Curriculo</p>
+            <p>Graduanda em Engenharia Elétrica pela Universidade Federal de Minas Gerais</p>
+            <p>Técnico em Eletrotécnica no Instituto Federal de Minas Gerais</p>
+        </div>
+    </div>
 </section>
 @endsection
