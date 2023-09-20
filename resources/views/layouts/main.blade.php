6da1af55820c261ae611d02be2616cd590d179ce
Rodfc773 <rodrigo.fc773@gmail.com>
2023-09-20 19:10:42 -0300

    finalizando o perfil público do professor

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,6 +12,7 @@
         <!-- CSS Bootstrap -->
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
         <!-- CSS da aplicação -->
         <link href="/css/header_style.css" rel="stylesheet">
--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -54,5 +54,79 @@
             <p>Técnico em Eletrotécnica no Instituto Federal de Minas Gerais</p>
         </div>
     </div>
+
+    <div class="avaliations">
+
+        <section class="classification">
+            <div class="student-avaliation">
+                    <p> <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star"></i>
+                        (4.0)
+                    </p>
+
+                <p class="text-avaliation student">"A Professora Juliana é incrível! Ela tornou a matemática divertida e me ajudou a melhorar minhas notas."</p>
+
+                <h5 class="name-student">Maria A.</h5>
+            </div>
+
+            <div class="student-avaliation">
+                    <p> <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        (5.0)
+                    </p>
+
+                <p class="text-avaliation student">""Aprendi mais com a Professora Juliana em um mês do que em um ano na escola.""</p>
+
+                <h5 class="name-student">João S.</h5>
+            </div>
+        </section>
+    </div>
+</section>
+
+<section class="agendar-aula">
+    <div class="marcar-aula">
+        <div class="marcar-up">
+            <p class="total-avaliation">
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-half"></i>
+                4,5 (23)
+            </p>
+        </div>
+        <hr>
+        <div class="resume-middle">
+
+            <p class="sessions-time">Sessões de 1 hora aula (50 minutos)</p>
+
+            <p>Especialidades</p>
+
+            <ul class="special-subjects">
+                <div>
+                    <li>Álgebra</li>
+                </div>
+
+                <div>
+                    <li>Geometria</li>
+                </div>
+            </ul>
+        </div>
+        <hr>
+        <div class="marcar-down">
+
+        <p id = "free-time">Próximo horário disponível</p>
+        <p id="time">14:00 - 29 de Agosto</p>
+
+
+        </div>
+        <button id="agendar" type ="button">Agendar Aula com Juliana</button>
+    </div>
 </section>
 @endsection
