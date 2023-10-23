ecdab28a4537c353eaf14604e04b612ad5cbb79a
Franklin Nascimento <waddinsohn@gmail.com>
2023-10-22 23:09:25 -0300

    adicionada a migration subtopicos e seus seeders

---
+++ database/migrations/2023_10_23_013537_create_subtopicos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subtopicos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('especialidade_id')->constrained(
+                'especialidades',
+                'id'
+            );
+            $table->string('descricao', 255);
+            $table->boolean('ativo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subtopicos');
+    }
+};
